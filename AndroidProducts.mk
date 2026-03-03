e473039c459a5a5f168c411a36ad50bffd8fa266
lovelyKoishi <Wanwan271@outlook.com>
2026-03-03 23:50:50 +0800

    你的提交信息

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_msm8998.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_msm8998-user \
+    omni_msm8998-userdebug \
+    omni_msm8998-eng
---
+++ omni_msm8998.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from msm8998 device
+$(call inherit-product, device/readboy/msm8998/device.mk)
+
+PRODUCT_DEVICE := msm8998
+PRODUCT_NAME := omni_msm8998
+PRODUCT_BRAND := readboy
+PRODUCT_MODEL := Readboy_C30
+PRODUCT_MANUFACTURER := readboy
+
+PRODUCT_GMS_CLIENTID_BASE := android-readboy
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="msm8998-user 10 QKQ1.190918.001 202108111744 dev-keys"
+
+BUILD_FINGERPRINT := readboy/msm8998/msm8998:10/QKQ1.190918.001/202108111744:user/dev-keys
